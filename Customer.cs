c1a060eb6e36a27d1c0d40728caaf9cd66641cbb
dinjuvjoseph <47115745+dinjuvjoseph@users.noreply.github.com>
2019-03-02 10:12:29 +0530

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVCdatabase.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Name")]
+        //[RegularExpression()]
+        public string Sname { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Address")]
+        [DataType(DataType.MultilineText)]
+        public string Saddress { get; set; }
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Email")]
+
+        public string Semail { get; set; }
+
+
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "User Name")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Cant be empty")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+
+    }
+}
